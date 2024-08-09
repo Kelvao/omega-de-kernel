9ebfbb78f581f1e5a14e5fb90233b6a4a3fad570
Kelvao <kelvintesche@gmail.com>
2024-08-09 03:04:55 -0300

    feat: test

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ set PATH=C:\devkitPro\msys\bin;C:\devkitPro\devkitARM\bin;%PATH%
 set DEVKITARM=/c/devkitPro/devkitARM
 set DEVKITPRO=/c/devkitPro
 set LIBGBA=/c/devkitPro/libgba
-
+echo devkitpro env setup completed
 make  
 pause
